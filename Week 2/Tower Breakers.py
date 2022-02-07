4e0679bfcb7c1a597001ccf765ff44f28ec2d0fb
Samuele <78924333+samacciu23@users.noreply.github.com>
2022-02-07 22:57:00 +0000

    Update Tower Breakers.py

--- Week 2/Tower Breakers.py
+++ Week 2/Tower Breakers.py
@@ -29,24 +29,6 @@ SAMPLE OUTPUT:
 2
 '''
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-#
-# Complete the 'towerBreakers' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER m
-#
-
 def towerBreakers(n, m):
     '''
     If height is equal to 1 then player 2 wins:
@@ -62,22 +44,3 @@ def towerBreakers(n, m):
     if n % 2 == 0 or m == 1:
         return 2
     return 1
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input().strip())
-
-    for t_itr in range(t):
-        first_multiple_input = input().rstrip().split()
-
-        n = int(first_multiple_input[0])
-
-        m = int(first_multiple_input[1])
-
-        result = towerBreakers(n, m)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
